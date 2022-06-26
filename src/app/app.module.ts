75f7de2e2cb2da87cd57568b8a7988cf30e8ddcf
claudio <solunojo@gmail.com>
2022-06-25 20:09:48 -0400

    Commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    TemplateFormsComponent,
+    ReactiveFormsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/palabra.ts
@@ -0,0 +1,6 @@
+export class Palabra {
+  constructor(
+    public id: number,
+    public name: string
+  ){ }
+}
